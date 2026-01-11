5b4d9ebdc86984a81190ff1c0cf4ab0554d1b512
Awez-Sheikh <awezsheikh007@gmail.com>
2026-01-11 17:21:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (91.28%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
-        string s=to_string(x); 
-        int n=s.size();
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            if(s[i]!=s[j]){
-                return false;
+            if(x<0) return false;
+            int n=x;
+            long long revnum=0;
+            while(n>0){
+                int ld=n%10;
+                revnum=revnum*10+ld;
+                n=n/10;
             }
-            i++;
-            j--;
-        }
-        return true;
-    }
+            return revnum == x;
+        }  
 };
