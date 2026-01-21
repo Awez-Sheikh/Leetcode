4b8c82e36f9ab74325ee3e0dd026893f8d443153
Awez-Sheikh <awezsheikh007@gmail.com>
2026-01-21 09:06:41 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.36%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0; // Pointer for inserting non-zero values
-        // Move all non-zero elements forward
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[j++] = nums[i];
+        int n=nums.size();
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
             }
         }
-        // Fill the rest with zeros
-        while (j < nums.size()) {
-            nums[j++] = 0;
+        while(j<n){
+            nums[j++]=0;
         }
     }
 };
+
