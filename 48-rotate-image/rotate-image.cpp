4b862685b50a2d4e31f0eb635d747e5105db7386
Awez-Sheikh <awezsheikh007@gmail.com>
2026-01-26 15:04:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (9.33%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-       int n=matrix.size();
-       for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            swap(matrix[i][j],matrix[j][i]);
+        // Get the size of the square matrix
+        int n = matrix.size();
+
+        // Create a new matrix of same size to store rotated result
+        vector<vector<int>> rotated(n, vector<int>(n));
+
+        // Traverse each element of original matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Place the element at its new rotated position
+                rotated[j][n - i - 1] = matrix[i][j];
+            }
         }
-       }
-       for(int i=0;i<n;i++){
-        reverse(matrix[i].begin(),matrix[i].end());
-       }
+        matrix = rotated;
     }
 };
