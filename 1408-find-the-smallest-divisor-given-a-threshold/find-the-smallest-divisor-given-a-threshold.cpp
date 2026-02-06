569e540d82551632a1e40be04ac5d7c7d3714ba2
Awez-Sheikh <awezsheikh007@gmail.com>
2026-02-06 21:39:17 +0530

    Time: 3 ms (92.15%) | Memory: 26.2 MB (48.03%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int s_o_d(vector<int>& nums, int div){
-        int sum=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            sum=sum+ceil((double)(nums[i])/(double)(div));
+    long long fnc(vector<int>& nums, int m) {
+        long long sum = 0; // Fix 2: Use long long
+        for(int i = 0; i < nums.size(); i++) {
+            // Fix 1 & 4: Integer ceiling division (a + b - 1) / b
+            sum += (nums[i] + m - 1) / m;
         }
         return sum;
     }
 
-
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int low=1;
-        int high = *max_element(nums.begin(),nums.end());
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(s_o_d(nums,mid)<=threshold){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+        int low = 1;
+        int high = *max_element(nums.begin(), nums.end());
+        int ans = high;
 
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(fnc(nums, mid) <= threshold) {
+                ans = mid;      
+                high = mid - 1; 
+            } else {
+                low = mid + 1;  
+            }
         }
-        return low;
+        return ans;
     }
 };
