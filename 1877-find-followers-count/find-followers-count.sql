8a7daa7f4c4efc711a84aa44841ef3b9325935a5
Awez-Sheikh <awezsheikh007@gmail.com>
2026-03-02 00:02:30 +0530

    Time: 536 ms (51.71%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-select user_id,
-sum(case when follower_id>='0' then 1 else 0 end) as followers_count
+select user_id, count(*) as followers_count
 from Followers
 group by user_id
-order by user_id asc
+order by user_id;
